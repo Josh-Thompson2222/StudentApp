9f58aaca15079cbb84e97f8469516a4367062a0a
Josh-Thompson2222 <118356604+Josh-Thompson2222@users.noreply.github.com>
2023-11-01 21:04:30 +0000

    class added called Main

--- src/main/java/ie/atu/StudentApp.java
+++ src/main/java/ie/atu/StudentApp.java
@@ -2,17 +2,22 @@ package ie.atu;
 
 import java.util.Scanner;
 
+/*This is a method; it defines the behaviour of the class
+ and prints out the information about the students*/
 public class StudentApp {
     private String name;
     private String email;
     private String course;
 
+    //Constructor - initializes the state of name, email and course
     public StudentApp(String name, String email, String course) {
         this.name = name;
         this.email = email;
         this.course = course;
     }
-
+    /*'Setters' and 'getters' are used to allow the user to input
+    * information, while the getters are used to call the data to
+    * display the info that has been previously set*/
     public StudentApp(String name) {
         this.name = name;
     }
